58e9d456ab1461b067b57f74cd799049841170b4
Елена Карпова <karpova.el.m@gmail.com>
2024-09-03 01:41:08 +0200

    45 passed

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from posts.models import Post, Group, Comment
 from rest_framework.response import Response
@@ -8,13 +7,14 @@ from rest_framework import status
 from rest_framework.permissions import BasePermission, IsAdminUser, IsAuthenticated
 
 from .serializers import PostSerializer, CommentSerializer, GroupSerializer
-import pdb
+
 
 class GroupUserPermission(BasePermission):
 
     def has_permission(self, request, view):
         if request.method in ['PUT', 'PATCH', 'POST', 'DELETE']:
             return request.user.is_staff
+            # return status.HTTP_405_METHOD_NOT_ALLOWED
         else:
             return True
 
@@ -38,23 +38,23 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    # queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = (PostCommentUserPermission, IsAuthenticated)
 
     def get_queryset(self):
         pk = self.kwargs['post_pk']
         queryset = Comment.objects.filter(post=pk)
-        # print(self.args)
-        # print(self.__dict__)
-        # post_pk = self.kwargs['pk']
         return queryset
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        serializer.save(
+            author=self.request.user,
+            post_id=self.kwargs.get('post_pk')
+        )
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (GroupUserPermission, IsAuthenticated)
+    # permission_classes = (IsAdminUser, )
