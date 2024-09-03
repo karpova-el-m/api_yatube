fc25663119554a918818b90295bde6f8a653fb40
Елена Карпова <karpova.el.m@gmail.com>
2024-09-03 03:19:49 +0200

    Done PEP8

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -107,7 +107,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
